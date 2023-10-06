bdc30d23d7263ea4375951de61b803b2005f8de7
prabhakaranthangavel <karangeetha9600@gmail.com>
2023-10-06 12:37:58 +0530

    Notes Added & console for Results

---
+++ 2 script.js
@@ -0,0 +1,128 @@
+//! Numbers::
+//! ===>Example:
+// var a = 10;
+// var b = 5.5;
+// console.log(a); //10
+
+// var a = 10;
+// var b = 5.5;
+// var c = a+b;
+// console.log(c); //15.5   
+
+// *************************************************************************************************************************
+
+//! String:
+//! ===>Example:
+
+//Method 1:
+// var a = "Prabha";
+// var b = "ERODE";
+// console.log(b); //"ERODE"  
+
+//Method 2:
+// var a = "Prabha";
+// var b = "ERODE";
+// console.log(a+b);//PrabhaERODE 
+
+//Method 3:
+// var a = 5.5;
+// var b = "Prabha";
+// var c = a + b;
+// console.log(c);//5.5Prabha
+
+//Method 4:
+// var a = "prabha";
+// var b = 5.5;
+// var c = a+b;
+// console.log(c);
+// console.log(typeof(c)); //prabha5.5
+                          //string      
+// var a = 5.5;
+// var b = 1991;
+// var c = "Prabha";
+// console.log(a+b+c);//1996.5Prabha
+
+// var a = "Prabha";
+// var b = 1991;
+// var c = 5.5;
+// console.log(a+b+c);//Prabha19915.5
+
+// var a;
+// a = 5.5;
+// a = "Prabha";
+// console.log(a);//Prabha
+
+// *************************************************************************************************************************
+
+//! Boolean:
+//! ===>Example:
+
+// var a = 10;
+// var b = 5.5;
+// var c = 10;
+// console.log(a==b); //false
+// console.log(a==c); //true
+// console.log(b==c); //false
+
+// *************************************************************************************************************************
+
+//! Array:
+//! ===>Example:
+
+// var myfamily = ["amma", "appa", "sister","brother"];
+// console.log(myfamily); // [ 'amma', 'appa', 'sister', 'brother' ]
+// console.log(myfamily[0]); //amma
+// console.log(myfamily[1]); //appa
+//console.log(myfamily[2]); //sister
+// console.log(myfamily[3]); //brother
+
+// *************************************************************************************************************************
+
+//! Object:
+//! ===>Example:
+
+// var a = {myName:"prabhakaran", motherName:"geetha"};
+// console.log(a); // { firstName: 'prabhakaran', lastName: 'geetha' }
+
+// var City = {
+//     Oldname : "Erodai",
+//     Newname  : "Erode",
+//     Year     : 1980 ,
+//     Favourite : "Turmeric",
+//     Leader  : "Periyar"
+//   };
+//   console.log(City);
+
+//    output:
+//    {
+//      Oldname: 'Erodai',
+//      Newname: 'Erode',
+//      Year: 1980,
+//      Favourite: 'Turmeric',
+//      Leader: 'Periyar' 
+//    }
+  
+// var a = 5.5;
+// var b = "Prabha";
+// var c = true;
+// var d = ["amma", "appa", "sister","brother"];
+// var e = {
+//     Oldname : "Erodai",
+//     Newname  : "Erode",
+//     Year     : 1980 ,
+//     Favourite : "Turmeric",
+//     Leader  : "Periyar"
+//   };
+//   var f;
+//   var g = undefined;
+//   var h = "";
+// console.log(typeof(a)); //number
+// console.log(typeof(b)); //string
+// console.log(typeof(c)); //boolean
+// console.log(typeof(d)); //object
+// console.log(typeof(e)); //object
+// console.log(typeof(f)); //undefined ===> A value not declared so it type is undefined.
+// console.log(typeof(g)); //undefined ===> Value is undefined, type is undefined.
+// console.log(typeof(h)); //string    ===> The value is "", the typeof is "string"
+
+// *************************************************************************************************************************
